d27be5badb74d6e3c45a41df6dbbb5381b5a70ca
Emily Jones <emjones829@gmail.com>
2020-06-05 03:52:05 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
-def unsafe?(speed)
-
+def unsafe?(num)
+  if num > 60
+    true
+  elsif num < 40
+    true
+  else false
+  end
 end
 
 
-
-def not_safe?(speed)
-	
+def not_safe?(num)
+  num < 40 || num > 60 ? true : false
 end
-	
 
 
