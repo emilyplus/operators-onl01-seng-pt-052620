f127fd60c7948186ab2c81d4b634824e01b5755f
Emily Jones <emjones829@gmail.com>
2020-06-09 13:45:33 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,29 @@
 def unsafe?(num)
   if num > 60
+<<<<<<< HEAD
     true
   elsif num < 40
     true
   else false
+=======
+    TRUE
+  elsif num < 40
+    TRUE
+  else FALSE
+>>>>>>> feb9faacc358f7f2d0e0de0be71dcd17ca4729b4
   end
 end
 
 
 def not_safe?(num)
+<<<<<<< HEAD
   num < 40 || num > 60 ? true : false
+=======
+  if num > 60 ? TRUE : FALSE
+  elsif num < 40 ? TRUE : FALSE
+  else FALSE
+  end
+>>>>>>> feb9faacc358f7f2d0e0de0be71dcd17ca4729b4
 end
 
 
